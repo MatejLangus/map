fd6c73c6734ecc2b425b6f6c84c8c3bd356820cc
Matej <Matej.langus@gmail.com>
2024-12-12 18:57:26 +0100

    compress

--- convert-gpx-to-geojson.js
+++ convert-gpx-to-geojson.js
@@ -29,12 +29,30 @@ if (gpxFiles.length === 0) {
     // Convert GPX to GeoJSON using gpx2geojson
     const geojson = gpx2geojson.gpx(doc);  // Pass the DOM object to gpx2geojson
 
+    const simplified = turf.simplify(geojson, { tolerance: 0.01, highQuality: false });
+
+    // Reduce the precision of coordinates
+    const precisionGeoJSON = geojsonPrecision(simplified, 5);  // Rounding to 5 decimal places
+  
+    // Minify the GeoJSON
+    const minifiedGeoJSON = JSON.stringify(precisionGeoJSON);
+  
+    // Compress the GeoJSON using gzip
+    const compressedGeoJSON = zlib.gzipSync(minifiedGeoJSON);
+  
+
+  
+  
+
+
+
+
     // Define the output file name
-    const outputFileName = file.replace('.gpx', '.geojson');
+    const outputFileName = file.replace('.gpx', '.geojson.gz');
     const outputFilePath = path.join(outputFolder, outputFileName);
 
     // Write the GeoJSON file
-    fs.writeFileSync(outputFilePath, JSON.stringify(geojson, null, 2));
+    fs.writeFileSync(outputFilePath, JSON.stringify(compressedGeoJSON, null, 2));
     console.log(`Converted ${file} to ${outputFileName}`);
   });
 

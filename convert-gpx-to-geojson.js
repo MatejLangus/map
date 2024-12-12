5609d58901f5daab7e290094e03353cc21a620e4
Matej <Matej.langus@gmail.com>
2024-12-12 19:34:48 +0100

    simplifiing

--- convert-gpx-to-geojson.js
+++ convert-gpx-to-geojson.js
@@ -33,10 +33,10 @@ if (gpxFiles.length === 0) {
     // Convert GPX to GeoJSON using gpx2geojson
     const geojson = gpx2geojson.gpx(doc);  // Pass the DOM object to gpx2geojson
 
-    const simplified = turf.simplify(geojson, { tolerance: 0.01, highQuality: false });
+    //const simplified = turf.simplify(geojson, { tolerance: 0.01, highQuality: false });
 
     // Reduce the precision of coordinates
-    const precisionGeoJSON = geojsonPrecision(simplified, 5);  // Rounding to 5 decimal places
+    const precisionGeoJSON = geojsonPrecision(geojson, 5);  // Rounding to 5 decimal places
   
     // Minify the GeoJSON
     const minifiedGeoJSON = JSON.stringify(precisionGeoJSON);

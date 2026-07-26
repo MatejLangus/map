912cf6a13740094266361cc7d571aeb3aa3182ba
Matej <Matej.langus@gmail.com>
2026-07-26 21:49:35 +0200

    remove my relive shortcut

--- assets/SiteConfig.js
+++ assets/SiteConfig.js
@@ -82,12 +82,5 @@ document.addEventListener('DOMContentLoaded', () => {
         }); 
     });
 
-    if (ReliveURL0 !== "") {
-        var reliveButton = document.createElement('a');
-        reliveButton.href = ReliveURL0;
-        reliveButton.target = '_blank';  // Open in a new tab
-        reliveButton.className = 'button relive-button';
-        reliveButton.style = "position: relative; background:orange";
-        document.getElementsByClassName("peak-tag")[0].appendChild(reliveButton);
-    }
+
 });
